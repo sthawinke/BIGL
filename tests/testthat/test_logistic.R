118c9a4486cd987667e294465f70c81d09de25f9
Maxim Nazarov <maxim.nazarov@openanalytics.eu>
2018-05-14 17:25:40 +0200

    fix L4 tests

--- tests/testthat/test_logistic.R
+++ tests/testthat/test_logistic.R
@@ -3,7 +3,7 @@ context("Log-logistic function")
 baseline <- 0
 asymptote <- 1
 
-expect_equal(L4(1, 1, baseline, asymptote, 2), 0.119202922)
+expect_equal(L4(1, 1, baseline, asymptote, log10(exp(2))), 0.119202922)
 
 ## Asymptote is attained under infinite dose
 expect_equal(L4(Inf, 1, baseline, asymptote, 2), asymptote)
@@ -12,5 +12,5 @@ expect_equal(L4(Inf, 1, baseline, asymptote, 2), asymptote)
 expect_equal(L4(0, 1, baseline, asymptote, 2), baseline)
 
 ## Check that log(EC50) works alright for current
-expect_equal(L4(pi, 1, baseline, asymptote, log(pi)),
+expect_equal(L4(pi, 1, baseline, asymptote, log10(pi)),
              (asymptote - baseline)/2)
